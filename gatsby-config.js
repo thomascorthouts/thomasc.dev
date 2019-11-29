4775e6086f9e32474d59e5773cf50a3f0f284965
Thomas Corthouts <thomas@datacamp.com>
2019-11-29 14:36:26 -0500

    Add some images

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#3260ab`,
         theme_color: `#3260ab`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/bitmoji-square.jpeg`, // This path is relative to the root of the site.
       },
     },
   ],
