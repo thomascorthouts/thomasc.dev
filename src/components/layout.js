2aaee8a2f2332392126865eff6c4d4876405ee03
Thomas Corthouts <thomas@datacamp.com>
2019-11-29 16:12:20 -0500

    Add emotion globals instead of css

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,9 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 /** @jsx jsx */
 import { ThemeProvider, jsx } from "theme-ui"
+import { Global, css } from "@emotion/core"
 
 import Header from "./header"
 import theme from "theme"
@@ -27,18 +21,16 @@ const Layout = ({ children }) => {
 
   return (
     <ThemeProvider theme={theme}>
+      <Global
+        styles={css`
+          body {
+            margin: 0;
+          }
+        `}
+      />
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>© {new Date().getFullYear()}, Built by Thomas Corthouts</footer>
-      </div>
+      <main>{children}</main>
+      <footer>© {new Date().getFullYear()}, Built by Thomas Corthouts</footer>
     </ThemeProvider>
   )
 }
