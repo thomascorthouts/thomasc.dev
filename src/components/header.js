c9a96721e035bcc078a6ff4197fe719305ee115c
Thomas Corthouts <thomas@datacamp.com>
2019-11-29 14:29:39 -0500

    First swap to thomasc.dev

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#3260ab`,
       marginBottom: `1.45rem`,
     }}
   >
