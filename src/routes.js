4882953210a420ef4dfa60241fc0b62c173137d7
Thomas Corthouts <thomas@datacamp.com>
2019-12-22 13:07:15 +0100

    chore(routes): add routes to have single source of truth

---
+++ src/routes.js
@@ -0,0 +1,4 @@
+// Social Links
+export const GITHUB_LINK = 'https://github.com/thomascorthouts'
+export const LINKEDIN_LINK = 'https://www.linkedin.com/in/thomascorthouts/'
+export const TWITTER_LINK = 'https://twitter.com/thomasc_dev'
