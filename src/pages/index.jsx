80dd32c33257c237118964be6cdf9d2ae0767a58
Thomas Corthouts <thomas@datacamp.com>
2019-12-22 13:09:54 +0100

    chore(index): add borderRadius to image

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
+import { Flex, Box } from '@theme-ui/components'
 import React from 'react'
 import { Link } from 'gatsby'
 
@@ -7,15 +8,33 @@ import Layout from 'components/layout'
 import Image from 'components/image'
 import SEO from 'components/seo'
 
+import ImageThomas from 'images/thomas.jpeg'
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div sx={{ maxWidth: 9, marginBottom: 3 }}>
-      <Image />
-    </div>
+    <Flex
+      sx={{
+        flexDirection: ['column-reverse', null, null, null, null, 'row'],
+      }}
+    >
+      <img
+        src={ImageThomas}
+        alt="Frontal image of Thomas' face"
+        sx={{
+          maxWidth: 9,
+          mx: [null, null, null, null, null, 5],
+          borderRadius: t => t.radii[0],
+        }}
+      />
+      <Box>
+        <h1>Hi y'all! 👋</h1>
+        <p sx={{ maxWidth: 13, fontSize: [2, null, 3] }}>
+          My name is Thomas Corthouts, I'm a full-stack software engineer who
+          currently works on the Growth Team at DataCamp.
+        </p>
+      </Box>
+    </Flex>
   </Layout>
 )
 
