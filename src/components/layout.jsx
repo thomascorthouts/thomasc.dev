9b699f3277f14e25b3a2f092830552415099ff83
Thomas Corthouts <thomas@datacamp.com>
2019-12-22 14:24:05 +0100

    chore(layout): remove unused graphql query

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,43 +2,30 @@
 import { ThemeProvider, jsx } from 'theme-ui'
 import React from 'react'
 import PropTypes from 'prop-types'
-import { useStaticQuery, graphql } from 'gatsby'
 import { Global, css } from '@emotion/core'
 
 import Header from './header'
 import Footer from './footer'
 import theme from 'theme'
 
-const Layout = ({ title, children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
+const Layout = ({ title, children }) => (
+  <ThemeProvider theme={theme}>
+    <Global
+      styles={css`
+        body {
+          margin: 0;
+          -webkit-font-smoothing: antialiased;
         }
-      }
-    }
-  `)
-
-  return (
-    <ThemeProvider theme={theme}>
-      <Global
-        styles={css`
-          body {
-            margin: 0;
-            -webkit-font-smoothing: antialiased;
-          }
-          svg {
-            fill: currentColor;
-          }
-        `}
-      />
-      <Header title={title} />
-      <main sx={{ color: 'greyDark', px: 3 }}>{children}</main>
-      <Footer />
-    </ThemeProvider>
-  )
-}
+        svg {
+          fill: currentColor;
+        }
+      `}
+    />
+    <Header title={title} />
+    <main sx={{ color: 'greyDark', px: 3 }}>{children}</main>
+    <Footer />
+  </ThemeProvider>
+)
 
 Layout.propTypes = {
   title: PropTypes.string,
