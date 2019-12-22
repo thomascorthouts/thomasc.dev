3c5e37c1c3ad93ca39275fd52500bce2f380fc7d
Thomas Corthouts <thomas@datacamp.com>
2019-12-22 13:06:38 +0100

    chore(footer): add socials and make it flex

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,23 +1,72 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
+import { Flex, Box, Image } from '@theme-ui/components'
 import PropTypes from 'prop-types'
 import React from 'react'
 
+import { GITHUB_LINK, LINKEDIN_LINK, TWITTER_LINK } from 'routes'
+
+import IconGithub from 'images/icons/github.svg'
+import IconLinkedIn from 'images/icons/linkedin.svg'
+import IconTwitter from 'images/icons/twitter.svg'
+
 const Footer = () => (
-  <footer
+  <Flex
+    as="footer"
     sx={{
       bg: 'blue',
       color: 'white',
-      py: 2,
       mt: 3,
+      py: 3,
       pl: 3,
+      flexDirection: 'column',
     }}
   >
-    © 2019, Built with{' '}
-    <a href="https://www.gatsbyjs.org" sx={{ color: 'white' }}>
-      Gatsby
-    </a>
-  </footer>
+    <Box sx={{ mx: 'auto' }}>
+      <a
+        href={GITHUB_LINK}
+        sx={{ color: 'greyLight', '&:hover, &:focus': { color: 'white' } }}
+      >
+        <IconGithub
+          sx={{
+            height: 24,
+            width: 24,
+            mx: 2,
+          }}
+        />
+      </a>
+      <a
+        href={LINKEDIN_LINK}
+        sx={{ color: 'greyLight', '&:hover, &:focus': { color: 'white' } }}
+      >
+        <IconLinkedIn
+          sx={{
+            height: 24,
+            width: 24,
+            mx: 2,
+          }}
+        />
+      </a>
+      <a
+        href={TWITTER_LINK}
+        sx={{ color: 'greyLight', '&:hover, &:focus': { color: 'white' } }}
+      >
+        <IconTwitter
+          sx={{
+            height: 24,
+            width: 24,
+            mx: 2,
+          }}
+        />
+      </a>
+    </Box>
+    <Box sx={{ mx: 'auto', mt: 2 }}>
+      © 2019, Built with{' '}
+      <a href="https://www.gatsbyjs.org" sx={{ color: 'white' }}>
+        Gatsby
+      </a>
+    </Box>
+  </Flex>
 )
 
 export default Footer
